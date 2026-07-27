1b9a87f984667f17576159875e2944880d826d47
DagChain <answervinod@gmail.com>
2026-07-27 15:09:09 +0530

    chore: add maitysabuj939@gmail.com to allowed admin emails

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,7 +10,7 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async signIn({ user }) {
-      const allowedEmails = ['answercannon@gmail.com', 'rrakeshhpandey@gmail.com'];
+      const allowedEmails = ['answercannon@gmail.com', 'rrakeshhpandey@gmail.com', 'maitysabuj939@gmail.com'];
       if (user.email && allowedEmails.includes(user.email)) {
         return true;
       }
